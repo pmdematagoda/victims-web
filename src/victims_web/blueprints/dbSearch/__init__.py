36b6e364dfd8360d7d67b3ebfda8edd714f94fa5
DulithaRanatunga <redhatdulitha@gmail.com>
2013-08-30 20:39:11 +1000

    Implemented advanced search. Functional, but ugly. To do:: - Implement persistent ordering on the option boxes. - Better UI

--- src/victims_web/blueprints/dbSearch/__init__.py
+++ src/victims_web/blueprints/dbSearch/__init__.py
@@ -49,50 +49,112 @@ def search(query=None):
     #
     acceptedFields = []#should match up with Hash in models.py}
     
-    allFields = dir(Hash)
-    for field in allFields:
-        if field not in excluded:
-            acceptedFields.append(field)
     
    """
-    acceptedFields = ['name','cves','submitter','format','vendor','status']
+    advanced="none"
+    acceptedFields = {'name':"",'cves':"",'submitter':"",'format':"",'vendor':"",'status':""}
     hashFields=['sha512','sha256','md5']
-    acceptedFields.extend(hashFields)
+    for hashType in hashFields:
+        acceptedFields[hashType]=""
     searchString=  ""
-    
     #Note to self:: look into Q objects for complex queries:
     #https://docs.djangoproject.com/en/dev/topics/db/queries/
     hashes = Hash.objects()
+    dateSearchValues={}
     if request.method == 'POST':
     #filter results
-        
-        searchField = request.form.get('field','name')
-        searchString = request.form.get('searchString','')
-        
-        if searchField in hashFields:
-            lookup = "hashes__%s__combined__icontains" % searchField
-            flash(lookup)
-            hashes = hashes.filter(**{lookup: searchString})
-        else:
-            if isinstance(getattr(Hash,searchField),StringField):
-                lookup = "%s__icontains" % searchField    
-            else:
-                lookup = searchField
-            hashes = hashes.filter(**{lookup: searchString})
+        if 'advSearch' in request.form:
+            #Advanced Search
+            from mongoengine.queryset import Q
+            #Create search filter on accepted fields
+            lookup = Q()
             
-        #keep search string and field after search
-        fieldId = acceptedFields.index(searchField)
-        #reorder dropdown menu
-        for i in xrange(fieldId):
-            acceptedFields[fieldId-i] = acceptedFields[fieldId-i-1]
-        acceptedFields[0] = searchField
-         
+            for field in acceptedFields.keys():
+                #getData
+                
+                searchString = request.form.get(field+"_searchString",'')
+                acceptedFields[field]= searchString
+                
+                if field in hashFields:
+                    field = "hashes__%s__combined" % field
+                
+                if len(searchString) > 0:
+                    option = request.form.get(field+"_searchOption","contains")
+                    if option == "contains":
+                       lookup = lookup & Q(**{"%s__icontains" % field:searchString})
+                    elif option == "exact":
+                       lookup = lookup & Q(**{"%s__iexact" % field:searchString})
+                    elif option == "any":
+                       for term in searchString.split():
+                        lookup = lookup | Q(**{"%s__icontains" % field:term})
+            
+            #Extend search filter to date time
+            day=request.form.get('date_day',"2013")
+            month=request.form.get('date_month',"1")
+            year=request.form.get('date_year',"1")
+            option=request.form.get('date_option','any')
+            dateSearchValues['date_day_val']=day
+            dateSearchValues['date_month_val']=month
+            dateSearchValues['date_year_val']=year
+            
+            day = 1 if day=="" else int(day)
+            month = 1 if month=="" else int(month)
+            year = 2013 if year=="" else int(year)
+            date = datetime.datetime(year,month,day)
+            if option =='on':
+                dateLookup = Q(submittedon=date)
+            elif option =='before':
+                dateLookup = Q(submittedon__lte=date)
+            elif option =='after':
+                dateLookup = Q(submittedon__gte=date)
+            else:
+                dateLookup = Q()
+                
+            
+            lookup = lookup  & dateLookup
+            
+            #filter by them
+            
+            
+            hashes = Hash.objects.filter(lookup)
+            advanced="block"
+        else:
+            #Simple Search        
+            searchField = request.form.get('field','name')
+            searchString = request.form.get('searchString','')
+            
+            if searchField in hashFields:
+                lookup = "hashes__%s__combined__icontains" % searchField
+                hashes = hashes.filter(**{lookup: searchString})
+            else:
+                if isinstance(getattr(Hash,searchField),StringField):
+                    lookup = "%s__icontains" % searchField    
+                else:
+                    lookup = searchField
+                hashes = hashes.filter(**{lookup: searchString})
+                
+            #keep search string and field after search
+            """
+            fieldId = acceptedFields.index(searchField)
+            #reorder dropdown menu
+            for i in xrange(fieldId):
+                acceptedFields[fieldId-i] = acceptedFields[fieldId-i-1]
+            acceptedFields[0] = searchField
+            
+            """
+            advanced="none"
+    
     
     data={
             'hashes': hashes,
             'numResults': len(hashes),
             'acceptedFields': acceptedFields,
-            'searchString': searchString
+            
+            'searchString': searchString,
+            'advanced':advanced
+            
         }
+        
+    data.update(dateSearchValues);    
 
     return render_template('search.html', **data)
